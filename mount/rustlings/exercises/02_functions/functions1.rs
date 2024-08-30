232ae7483684e6671541fdd5faea072f5dfd1307
Derek Garcia <dcia811@gmail.com>
2024-08-30 12:53:09 -1000

    feat(functions): complete exercise 2

--- mount/rustlings/exercises/02_functions/functions1.rs
+++ mount/rustlings/exercises/02_functions/functions1.rs
@@ -1,5 +1,9 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me() {
+    println!("Hello World!");
+}
+
 fn main() {
     call_me(); // Don't change this line
 }
