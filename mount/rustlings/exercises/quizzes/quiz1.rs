989590fdd7d995f18f791fe08b2ec87d5c071fa0
Derek Garcia <dcia811@gmail.com>
2024-08-30 14:34:53 -1000

    feat(quiz): complete quiz 1

--- mount/rustlings/exercises/quizzes/quiz1.rs
+++ mount/rustlings/exercises/quizzes/quiz1.rs
@@ -11,6 +11,14 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+static APPLE_COST: i32 = 2;
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        num_apples
+    } else {
+        num_apples * APPLE_COST
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
