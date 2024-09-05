b7552463104c06873fe884e1c8f0365fc71f2452
Derek Garcia <dcia811@gmail.com>
2024-09-05 11:21:21 -1000

    feat(move semantics): complete exercise 6

--- mount/rustlings/exercises/06_move_semantics/move_semantics2.rs
+++ mount/rustlings/exercises/06_move_semantics/move_semantics2.rs
@@ -18,9 +18,9 @@ mod tests {
     // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
-        let vec0 = vec![22, 44, 66];
+        let mut vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
