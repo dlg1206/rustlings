4bbb99b0731debec6ea7f6eab5d78b1726c4f0a5
Derek Garcia <dcia811@gmail.com>
2024-10-03 09:31:29 -1000

    feat(generics): complete excercise 14

--- mount/rustlings/exercises/14_generics/generics2.rs
+++ mount/rustlings/exercises/14_generics/generics2.rs
@@ -1,12 +1,14 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // TODO: Rewrite it using a generic so that it supports wrapping ANY type.
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
 // TODO: Adapt the struct's implementation to be generic over the wrapped value.
-impl Wrapper {
-    fn new(value: u32) -> Self {
+// impl<T> - implementation is generic over type T, ie can hold any value
+// Wrapper<T> - from struct declaration
+impl<T> Wrapper<T> {
+    fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
