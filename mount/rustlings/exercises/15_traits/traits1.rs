126ef5e34566e9f033ac5185956cfefe1b3bf819
Derek Garcia <dcia811@gmail.com>
2024-10-09 10:41:15 -1000

    feat(traits): complete exercise 15

--- mount/rustlings/exercises/15_traits/traits1.rs
+++ mount/rustlings/exercises/15_traits/traits1.rs
@@ -6,6 +6,10 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(mut self) -> String {
+        self += &String::from("Bar");
+        self
+    }
 }
 
 fn main() {
--- mount/rustlings/exercises/15_traits/traits5.rs
+++ mount/rustlings/exercises/15_traits/traits5.rs
@@ -19,7 +19,8 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+// You can use '+' to chain traits together
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
